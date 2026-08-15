0b0a3dbc5ea4eec6aa4d2d80346290b92d87494e
Joss220r <jayalaf1@miumg.edu.gt>
2026-08-14 20:31:38 -0600

    add: inicial py

---
+++ main.py
@@ -0,0 +1,11 @@
+from queue import Queue
+from threading import Event, Lock, Thread
+
+
+def runApplication():
+    print("NovaTech - Procesamiento concurrente de pedidos")
+    print("ETAPA 1: proyecto iniciado correctamente.")
+
+
+if __name__ == "__main__":
+    runApplication()
