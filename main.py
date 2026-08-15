2ed339c325358d987eee7996ba4c953c75bc39ec
Joss220r <jayalaf1@miumg.edu.gt>
2026-08-14 20:41:13 -0600

    Add: initial inventory loaded

--- main.py
+++ main.py
@@ -2,9 +2,44 @@ from queue import Queue
 from threading import Event, Lock, Thread
 
 
+def createInitialInventory():
+    return {
+        "P001": {
+            "productName": "Teclado mecanico",
+            "stock": 12,
+        },
+        "P002": {
+            "productName": "Mouse inalambrico",
+            "stock": 18,
+        },
+        "P003": {
+            "productName": "Audifonos USB",
+            "stock": 10,
+        },
+        "P004": {
+            "productName": "Camara web",
+            "stock": 8,
+        },
+        "P005": {
+            "productName": "Monitor de 24 pulgadas",
+            "stock": 6,
+        },
+    }
+
+
+def showInventory(inventory):
+    print("\nInventario inicial:")
+    for productCode, productData in inventory.items():
+        productName = productData["productName"]
+        stock = productData["stock"]
+        print(f"{productCode} | {productName} | {stock} unidades")
+
+
 def runApplication():
     print("NovaTech - Procesamiento concurrente de pedidos")
-    print("ETAPA 1: proyecto iniciado correctamente.")
+    print("ETAPA 2: inventario cargado correctamente.")
+    inventory = createInitialInventory()
+    showInventory(inventory)
 
 
 if __name__ == "__main__":
